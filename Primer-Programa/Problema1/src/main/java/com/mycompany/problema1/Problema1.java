2510dd6b057a8bc1f292a1ec8cc36b1c1bd8fa3a
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2024-03-27 09:03:08 -0500

    nombre y direccion

---
+++ Primer-Programa/Problema1/src/main/java/com/mycompany/problema1/Problema1.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.problema1;
+
+/**
+  Escribir y ejecutar un programa que imprima su nombre y dirección
+ * 
+ */
+public class Problema1 {
+
+    public static void main(String[] args) { 
+        String nombre = "Jefferson Javier Ayovi Corozo ";
+        String direccion = "Isla Trinitaria";
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Direccion: "+ direccion);
+        
+    }
+}
