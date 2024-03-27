8576813d3591618fdbae560682f2d3c6d2a6046e
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2024-03-27 15:52:42 -0500

    sentencia lógica (boolean)

---
+++ Operadores/Problema6/src/main/java/com/mycompany/problema6/Problema6.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.problema6;
+
+/**
+ * Escribir una sentencia lógica (boolean) que clasifique un entero x en una de las siguientes categorías
+ *  
+ * 
+ */
+public class Problema6 {
+
+    public static void main(String[] args) {
+        int x = 50; // Cambia este valor para probar con diferentes números
+
+        if (x < 0) {
+            System.out.println("x es menor que 0");
+        } else if (x >= 0 && x <= 100) {
+            System.out.println("x está entre 0 y 100, inclusive");
+        } else {
+            System.out.println("x es mayor que 100");
+        }
+            
+        
+        
+        
+        
+        
+    }
+}
