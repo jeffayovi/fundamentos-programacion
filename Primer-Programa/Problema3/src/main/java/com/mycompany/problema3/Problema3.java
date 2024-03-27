c29fef34fec64f2e3d9cb8c26d1c9400ab97e31d
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2024-03-27 09:19:15 -0500

    imprimir la letra B

---
+++ Primer-Programa/Problema3/src/main/java/com/mycompany/problema3/Problema3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.problema3;
+
+/**
+ *Escribir un programa que imprima una letra B con asteriscos, de la siguiente manera:
+ * 
+ */
+public class Problema3 {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("******");
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("******");
+    }
+}
