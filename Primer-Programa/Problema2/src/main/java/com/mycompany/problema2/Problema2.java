1e5bd36d0db8124f6687d69dda0bb104e13a15eb
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2024-03-27 09:11:40 -0500

    parrafo de Principito

---
+++ Primer-Programa/Problema2/src/main/java/com/mycompany/problema2/Problema2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.problema2;
+
+/**
+ *Redactar y ejecutar un programa que imprima una página de texto con no más de 40 
+  caracteres por línea.
+ * 
+ */
+public class Problema2 {
+
+    public static void main(String[] args) {
+        System.out.println("Los ojos están ciegos. Es necesario buscar con el corazón");
+        System.out.println("Ya se trate de la casa, de las estrellas o del desierto, lo que los embellece es invisible");
+        System.out.println("He aquí mi secreto. Es muy simple: no se ve bien sino con el corazón. Lo esencial es invisible a los ojos");
+        System.out.println("Puede que haya millones de rosas en todo el mundo, pero tú eres mi única y exclusiva rosa");
+        System.out.println("Cuando uno está muy triste son agradables las puestas de sol");
+        System.out.println("El Principito, de Antoine de Saint Exupery");
+    }
+}
